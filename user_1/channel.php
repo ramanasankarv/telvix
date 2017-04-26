70be0e72b4cb8282f722b52850d864472a0ddb7d
Ramana <ramanasankarv@gmail.com>
2017-04-26 20:46:10 +0530

    css

--- user_1/channel.php
+++ user_1/channel.php
@@ -66,9 +66,7 @@ $page="channel";
         <div class="col-md-9">
          	<div class="row">
             	<div class="content-box-large">
-              		<div class="panel-heading">
-
-              		</div>
+              		
               		<div class="panel-body">
                   
                 		<div class="">
